5246df97d8b90c53702a6ee36355dfbad43524c7
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2023-03-31 13:16:26 +0700

    Membuat prosedur Quick sort

--- QuickSort_155/QuickSort_155.cpp
+++ QuickSort_155/QuickSort_155.cpp
@@ -20,7 +20,7 @@ void input() {
     }
 
     cout << "\n =======================" << endl;
-    cout << "\n Enter Array Element" << endl;
+    cout << "\n  Enter Array Element" << endl;
     cout << "\n =======================" << endl;
 
     for (int i = 0; i < n; i++)
@@ -43,10 +43,64 @@ void q_sort(int low, int high)
     int pivot, i, j;
     if (low > high) // langkah 1
         return;
+    // partition the list into two part
+    // one containing element less that or equal to pivot
+    // outher containing element greather than pivot
+
+    i = low + 1; // langkah 3
+    j = high; // langkah 4
+    pivot = arr[low]; // langkah 2
+
+    while (i <= j) // langkah 10
+    {
+        // search for element greather than pivot
+        while ((arr[i] <= pivot) && (i <= high)) // langkah 5
+        {
+            i++; // langkah 6
+            cmp_count++;
+        }
+        cmp_count++;
+        // search for an element less than or equal to pivot
+        while ((arr[j] > pivot) && (j >= low)) // langkah 7
+            j--; //langkah 8
+            cmp_count++;
+    }
+    cmp_count++;
+    if (i < j) // langkah 9
+        // if greater element is on left of the element 
+    {
+        // swap the element at index  i with the element at index j
+        swap(i, j); // langkah 11
+        mov_count++;
+    }
+    // j now containt the index of the last element in the sort list
+    if (low < j) // langkah 11
+        // move  to the pivot to its correct  position in the list
+    {
+        swap(low, j);
+        mov_count++;
+    }
+
+    // sort the list on the left of pivot using quick sort
+    q_sort(low, j - 1); // langkah 12
+    // sort the list on the right of pivot using quick sort
+    q_sort(j + 1, high); // langkah 13
+}
+
+void display() {
+    cout << "\n=============================" << endl;
+    cout << "\n Sorted Array"<< endl;
+    cout << "\n=============================" << endl;
+
+    for (int i = 0; 1 < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n\n Number of comparision " << cmp_count << endl;
+    cout << "\n\n Number of data movement " << mov_count << endl;
 }
 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    input();
 }
