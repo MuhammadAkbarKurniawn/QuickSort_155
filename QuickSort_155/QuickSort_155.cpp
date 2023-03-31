cbf6618427e3d08b24fdc120132f17ad6e10839d
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2023-03-31 10:58:55 +0700

    membuat variabel array, perbandingan dan perpindahan array

--- QuickSort_155/QuickSort_155.cpp
+++ QuickSort_155/QuickSort_155.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 
+// array of integers to hold values
+int arr[20];
+int cmp_count = 0;  // number of comparision
+int mov_count = 0; // number of movement
+int n;
+
+
 int main()
 {
     std::cout << "Hello World!\n";
